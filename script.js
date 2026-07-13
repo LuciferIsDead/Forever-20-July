ebf4970d28b24a47395c5c9996fdd8e2a914559d
LuciferIsDead <shahidul.sumon69@gmail.com>
2026-07-13 12:43:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const reliveBtn = document.getElementById("reliveBtn");
+const homeScreen = document.getElementById("homeScreen");
+const storyScreen = document.getElementById("storyScreen");
+
 alert("script.js loaded");
+
+reliveBtn.addEventListener("click", () => {
+    alert("Button clicked");
+
+    homeScreen.style.display = "none";
+    storyScreen.style.display = "flex";
+});
