f1b19b3b4cd8a75c4de8612706d07e06daec0c8b
LuciferIsDead <shahidul.sumon69@gmail.com>
2026-07-13 23:00:32 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,86 @@
+// =====================================
+// ELEMENTS
+// =====================================
+
+const reliveBtn = document.getElementById("reliveBtn");
+
+const homeScreen = document.getElementById("homeScreen");
+const storyScreen = document.getElementById("storyScreen");
+
+const chapterTitle = document.getElementById("chapterTitle");
+const chapterText = document.getElementById("chapterText");
+
+const nextStory = document.getElementById("nextStory");
+
+// =====================================
+// OPEN STORY
+// =====================================
+
+reliveBtn.addEventListener("click", () => {
+
+    homeScreen.classList.remove("show");
+    homeScreen.classList.add("hidden");
+
+    storyScreen.classList.remove("hidden");
+    storyScreen.classList.add("show");
+
+});
+
+// =====================================
+// CHAPTER 1
+// =====================================
+
+chapterTitle.textContent = "The Night Everything Changed";
+
+chapterText.innerHTML = `
+It all began with one tiny Instagram Note...
+
+<br><br>
+
+💭 <b>Ghumai.</b>
+
+<br><br>
+
+💙 <b>Ghuman.</b>
+
+<br><br>
+
+Neither of us knew that one little reply
+would slowly become the most beautiful
+story of our lives.
+`;
+
+// =====================================
+// NEXT BUTTON
+// =====================================
+
+nextStory.addEventListener("click", () => {
+
+    chapterTitle.textContent = "Chapter 02";
+
+    chapterText.innerHTML = `
+The story continues...
+
+<br><br>
+
+Version 1.2 will include:
+
+<br><br>
+
+• Instagram Group Chat
+
+<br>
+
+• Samuel
+
+<br>
+
+• Fake Couple
+
+<br>
+
+• First Feelings ❤️
+
+`;
+
+});
